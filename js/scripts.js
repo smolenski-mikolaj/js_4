bafd59ee502cdb8db3b2b446fe0f9a6d07e9088a
smolenski-mikolaj <31181850+smolenski-mikolaj@users.noreply.github.com>
2017-08-31 12:31:00 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function drawTree(h) {
+	
+	for ( var i = 0; i <= h; i++) {
+		var star = "";
+		for ( var j = 0; j <= i; j++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+	
+}
+
+drawTree(6);
